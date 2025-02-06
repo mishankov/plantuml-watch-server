bebe8ebf4342cb0868f9fadada362c2ccb3e6728
Denis Mishankov <mishankov@mail.com>
2025-02-07 02:26:50 +0300

    add ws handler for svg changes

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/mishankov/plantuml-watch-server
 
 go 1.23.0
+
+require github.com/gorilla/websocket v1.5.3
--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"os/exec"
+
+	"github.com/gorilla/websocket"
 )
 
 func runPlantUML(input, output string) {
@@ -43,6 +46,63 @@ func server() {
 
 		w.Header().Add("Content-Type", "text/html")
 		w.Write(svg)
+	})
+
+	// Hanler function to stream updates
+	http.HandleFunc("/ws/{name}", func(w http.ResponseWriter, r *http.Request) {
+		_, cancel := context.WithCancel(r.Context())
+		defer cancel()
+
+		svgName := r.PathValue("name")
+		svgFullPath := fmt.Sprintf("/output/%v.svg", svgName)
+
+		svg, err := os.ReadFile(svgFullPath)
+		if err != nil {
+			w.WriteHeader(404)
+			w.Write([]byte("SVG not found. Error: " + err.Error()))
+			return
+		}
+
+		var upgrader = websocket.Upgrader{
+			ReadBufferSize:  1024,
+			WriteBufferSize: 1024,
+			CheckOrigin:     func(_ *http.Request) bool { return true },
+		}
+
+		ws, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			w.WriteHeader(500)
+			w.Write([]byte("Couldn't upgrade to WebSocker. Error: " + err.Error()))
+			return
+		}
+
+		go func() {
+			for {
+				if _, _, err := ws.NextReader(); err != nil {
+					log.Println("Couldn't get NextReader:", err)
+					ws.Close()
+					cancel()
+					break
+				}
+			}
+		}()
+
+		ws.WriteMessage(1, svg)
+
+		for {
+			err := watchFile(svgFullPath)
+			if err != nil {
+				log.Println(err)
+				break
+			}
+
+			log.Println("SVG changed:", svgFullPath)
+
+			svg, _ := os.ReadFile(svgFullPath)
+			if len(svg) != 0 {
+				ws.WriteMessage(1, svg)
+			}
+		}
 	})
 
 	log.Println("http://localhost:8080/")
